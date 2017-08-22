9576c70cda3dda46642cfc504f7ba0727f9f29ac
Jiyun Wan <rosemary1171010@gmail.com>
2017-08-22 20:18:10 +1200

    Update test_validator.py

--- PBash/test_validator.py
+++ PBash/test_validator.py
@@ -3,20 +3,28 @@ from validator import Validator
 
 
 class ValidatorTests(unittest.TestCase):
+    # for good day scenarios
     global v
     v = Validator()
 
     def test_id(self):
         self.assertTrue(v.check_id('UY7'))
+        self.assertTrue(v.check_id('000'))
+        self.assertTrue(v.check_id('AAA'))
+        self.assertTrue(v.check_id('999'))
 
     def test_gender(self):
         self.assertTrue(v.check_gender('M'))
+        self.assertTrue(v.check_gender('F'))
 
     def test_age(self):
-        pass
+        self.assertTrue(v.check_age(1))
+        self.assertTrue(v.check_age(99))
 
     def test_sales(self):
-        pass
+        self.assertTrue(v.check_sales(1))
+        self.assertTrue(v.check_sales(99))
+        self.assertTrue(v.check_sales(998))
 
     def test_bmi(self):
         pass

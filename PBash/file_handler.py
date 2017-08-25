08e7c88911d8dbe4689da61f0cfcd63b5c75a3aa
Jiyun Wan <rosemary1171010@gmail.com>
2017-08-25 21:07:26 +1200

    Update file_handler.py

--- PBash/file_handler.py
+++ PBash/file_handler.py
@@ -102,13 +102,30 @@ class FileHandler:
             print('The file was not found', file=sys.stderr)
             return False
 
-    def open_help(self, subject):
-        pass
-        # open the file (hard-coded file path)
-        # split the file into entries
-        # check the entries for the subject
-        # if found, return the result for that subject
-
+    # Rosemary
+    def open_help(self, help_command):
+        """
+        >>> f = FileHandler(new_validator=Validator)
+        >>> print(f.open_help('line'))
+        line command vitualize the data.
+        >>> print(f.open_help('help'))
+        help command brings out all command.
+        >>> print(f.open_help('helpp'))
+        No such command.
+        """
+        try:
+            file = open("help.txt", "r")
+            for line in file:
+                if len(line.split("=")) == 2:
+                    entries = line.split("=")
+                    if help_command == entries[0]:
+                        return entries[1].rstrip('\n')
+                else:
+                    print("Invalid help file format!")
+        except FileNotFoundError:
+            print('The help file was not found', file=sys.stderr)
+        return "No such command."
+        
     def excel_reader(self, filename):
         """
         >>> f = FileHandler(Validator())
